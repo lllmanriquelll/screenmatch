403d9e7d6c12ef6566cb61cad9a91caca192fdf1
Luis Manrique <manrique.luis@gmail.com>
2024-05-16 14:02:32 -0300

    Primeiro commit

---
+++ src/main/java/com/example/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package com.example.screenmatch;
+
+import com.example.screenmatch.model.DadosSerie;
+import com.example.screenmatch.service.ConsumoApi;
+import com.example.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("Primeiro projeto Spring!");
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?i=tt2575988&apikey=f48b743");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
